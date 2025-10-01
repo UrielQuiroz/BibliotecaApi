bda878b1e892f87beecbc1dc386007cd1d3c2113
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-10-01 11:41:03 -0600

    CRUD Autores y libros, EF Core, Reglas de ruteo

---
+++ BibliotecaAPI/Controllers/AutoresController.cs
@@ -0,0 +1,87 @@
+﻿using BibliotecaAPI.Datos;
+using BibliotecaAPI.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BibliotecaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet("/listado-de-autores")]
+        [HttpGet]
+        public async Task<IEnumerable<Autor>> Get()
+        {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpGet("primero")]
+        public async Task<Autor> GetPrimerAutor()
+        {
+            return await context.Autores.FirstAsync();
+        }
+
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Autor>> Get(int id)
+        {
+            var autor = await context.Autores
+                .Include(x => x.Libros)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if(autor is null)
+            {
+                return NotFound();
+            }
+
+            return autor;
+
+        }
+
+        [HttpGet("{parametro1}/{parametros2?}")]
+        public ActionResult Get(string parametro1, string parametros2 = "Valor por defecto")
+        {
+            return Ok(new { parametro1, parametros2 });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, Autor autor)
+        {
+            if (id != autor.Id) {
+                return BadRequest("Los ids deben de coincidir");
+            }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var registrosBorrados = await context.Autores.Where(x => x.Id == id).ExecuteDeleteAsync();
+
+            if(registrosBorrados == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ BibliotecaAPI/Program.cs
@@ -0,0 +1,21 @@
+using BibliotecaAPI.Datos;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Area de servicios
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddDbContext<ApplicationDbContext>( options => 
+    options.UseSqlServer("name=DefaultConnection"));
+
+var app = builder.Build();
+
+//Area de middlewares
+
+app.MapControllers();
+
+app.Run();
