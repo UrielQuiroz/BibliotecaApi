08862a43ae237a7bda78fa056e021dc0cdf5862c
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-10-01 12:22:58 -0600

    DataAnnotations - Validaciones por defecto

--- BibliotecaAPI/Entidades/Autor.cs
+++ BibliotecaAPI/Entidades/Autor.cs
@@ -5,9 +5,18 @@ namespace BibliotecaAPI.Entidades
     public class Autor
     {
         public int Id { get; set; }
-        [Required]
+        //[Required(ErrorMessage = "El campo Nombre es requerido")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(10, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
         public required string Nombre { get; set; }
 
         public List<Libro> Libros { get; set; } = new List<Libro>();
+
+        [Range(18, 120)]
+        public int Edad { get; set; }
+        [CreditCard]
+        public string? TarjetaDeCredito { get; set; }
+        [Url]
+        public string? URL { get; set; }
     }
 }
