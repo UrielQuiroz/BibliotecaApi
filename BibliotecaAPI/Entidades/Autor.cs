e2fde792eeae961e1f5965e5950d54fd1d166e59
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-10-01 12:51:54 -0600

    Validaciones Personalizadas por Modelo

--- BibliotecaAPI/Entidades/Autor.cs
+++ BibliotecaAPI/Entidades/Autor.cs
@@ -3,16 +3,30 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BibliotecaAPI.Entidades
 {
-    public class Autor
+    public class Autor : IValidatableObject
     {
         public int Id { get; set; }
         //[Required(ErrorMessage = "El campo Nombre es requerido")]
         [Required(ErrorMessage = "El campo {0} es requerido")]
-        [StringLength(150, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
-        [PrimeraLetraMayuscula]
+        [StringLength(10, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
+        //[PrimeraLetraMayuscula]
         public required string Nombre { get; set; }
 
         public List<Libro> Libros { get; set; } = new List<Libro>();
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                var primeraLetra = Nombre[0].ToString();
+
+                if (primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayuscula - por modelo",
+                        new string[] { nameof(Nombre) });
+                }
+            }
+        }
 
         //[Range(18, 120)]
         //public int Edad { get; set; }
