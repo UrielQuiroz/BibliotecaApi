9f6cecc4534b36ead95424a35619695bd1870636
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-10-03 13:17:52 -0600

    Relación Uno a Muchos - Tabla Comentarios

--- BibliotecaAPI/Entidades/Libro.cs
+++ BibliotecaAPI/Entidades/Libro.cs
@@ -11,5 +11,6 @@ namespace BibliotecaAPI.Entidades
         public required string Titulo { get; set; }
         public int AutorId { get; set; }
         public Autor? Autor { get; set; }
+        public List<Comentario> Comentarios { get; set; } = [];
     }
 }
