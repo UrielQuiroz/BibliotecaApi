abd62458e5a879f4260312f6b06ccb81373ed39d
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-10-03 16:52:09 -0600

    Creando una Colección de Recursos. Un autor o mas con libros

--- BibliotecaAPI/DTOs/AutorCreateDTO.cs
+++ BibliotecaAPI/DTOs/AutorCreateDTO.cs
@@ -17,6 +17,6 @@ namespace BibliotecaAPI.DTOs
 
         [StringLength(20, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
         public required string Identificacion { get; set; }
-        public List<LibroCreateDTO> Libros { get; set; }
+        public List<LibroCreateDTO>? Libros { get; set; }
     }
 }
