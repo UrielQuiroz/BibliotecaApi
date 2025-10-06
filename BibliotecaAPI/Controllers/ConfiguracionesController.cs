82f27bfa8236d92cf97cd1baec02f43f8f16373e
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-10-06 15:30:27 -0600

    Configuraciones - IConfiguration

---
+++ BibliotecaAPI/Controllers/ConfiguracionesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BibliotecaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/configuraciones")]
+    public class ConfiguracionesController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public ConfiguracionesController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var opcion1 = configuration["MejorJugador"]!;
+            var opcion2 = configuration.GetValue<string>("MejorJugador")!;
+            return opcion2;
+        }
+
+        [HttpGet("secciones")] 
+        public ActionResult<string> GetSeccion()
+        {
+            var opcion1 = configuration["ConnectionStrings:DefaultConnection"]!;
+            var opcion2 = configuration.GetValue<string>("ConnectionStrings:DefaultConnection")!;
+
+            var seccion = configuration.GetSection("ConnectionStrings");
+            var opcion3 = seccion["DefaultConnection"]!;
+
+            return opcion3;
+        }
+    }
+}
