90917d6cf6af14d08dac2d5d8dadf4a5af79ce4a
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-10-08 13:11:33 -0600

    Registrando Usuarios y construyendo token de autenticacion

---
+++ BibliotecaAPI/Controllers/UsuariosController.cs
@@ -0,0 +1,83 @@
+﻿using BibliotecaAPI.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BibliotecaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/usuarios")]
+    [Authorize]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly IConfiguration configuration;
+
+        public UsuariosController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.configuration = configuration;
+        }
+
+        [HttpPost("registro")]
+        [AllowAnonymous]
+        public async Task<ActionResult<RespuestaAutenticacionDTO>> Registrar(CredencialesUsuariosDTO credencialesUsuariosDTO)
+        {
+            var usuario = new IdentityUser
+            {
+                UserName = credencialesUsuariosDTO.Email,
+                Email = credencialesUsuariosDTO.Email
+            };
+
+            var resultado = await userManager.CreateAsync(usuario, credencialesUsuariosDTO.Password!);
+
+            if (resultado.Succeeded)
+            {
+                var respuestaAutenticacion = await construirToken(credencialesUsuariosDTO);
+                return respuestaAutenticacion;
+            } else
+            {
+                foreach(var error in resultado.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+
+                return ValidationProblem();
+            }
+        }
+
+        private async Task<RespuestaAutenticacionDTO> construirToken(CredencialesUsuariosDTO credencialesUsuariosDTO)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim("email", credencialesUsuariosDTO.Email),
+                new Claim("cualquier llave", "cualquier valor")
+,            };
+
+            var usuario = await userManager.FindByEmailAsync(credencialesUsuariosDTO.Email);
+            var claimsDB = await userManager.GetClaimsAsync(usuario!);
+
+            claims.AddRange(claimsDB);
+
+            var llave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["jwtKey"]!));
+            var credenciales = new SigningCredentials(llave, SecurityAlgorithms.HmacSha256);
+
+            var expiracion = DateTime.UtcNow.AddDays(1);
+
+            var tokenDeSeguridad = new 
+                JwtSecurityToken(issuer: null, audience: null, claims: claims, expires: expiracion, signingCredentials: credenciales);
+
+            var token = new JwtSecurityTokenHandler().WriteToken(tokenDeSeguridad);
+
+            return new RespuestaAutenticacionDTO
+            {
+                Token = token,
+                Expiracion = expiracion
+            };
+        }
+    }
+}
