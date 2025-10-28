b634fe0dce90f5becf7c59f572f236d707eae6f7
UrielQuiroz <55451119+UrielQuiroz@users.noreply.github.com>
2025-10-28 10:49:34 -0600

    Implementando HATEOAS

---
+++ BibliotecaAPI/Controllers/V1/RootController.cs
@@ -0,0 +1,24 @@
+﻿using BibliotecaAPI.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BibliotecaAPI.Controllers.V1
+{
+    [ApiController]
+    [Route("api/v1")]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = "ObtenerRootV1")]
+        public IEnumerable<DatosHATEOASDTO> Get()
+        {
+            var datosHATEOAS = new List<DatosHATEOASDTO>();
+
+            datosHATEOAS.Add(new DatosHATEOASDTO(
+                Enlace: Url.Link("ObtenerRootV1", new { })!,
+                Descripcion: "self",
+                Metodo: "GET"
+            ));
+
+            return datosHATEOAS; 
+        }
+    }
+}
